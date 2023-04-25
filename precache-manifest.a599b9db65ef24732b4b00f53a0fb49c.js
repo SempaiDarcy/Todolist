fab349fa6e09df21e7e8e16077019457392dc223
SempaiDarcy <mr_darcy_1@mail.ru>
2023-04-25 06:11:38 +0500

    Updates

---
+++ precache-manifest.a599b9db65ef24732b4b00f53a0fb49c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af0f2b3fb75bf3e0819d644af08181e",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "27b42a8521405bafab2e",
+    "url": "/Todolist/static/css/2.f0e33c08.chunk.css"
+  },
+  {
+    "revision": "1e6a4e3a556b51fceb46",
+    "url": "/Todolist/static/css/main.ac46c049.chunk.css"
+  },
+  {
+    "revision": "27b42a8521405bafab2e",
+    "url": "/Todolist/static/js/2.3ee8db38.chunk.js"
+  },
+  {
+    "revision": "ee84377a6837b87d0603589943d32d20",
+    "url": "/Todolist/static/js/2.3ee8db38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6a4e3a556b51fceb46",
+    "url": "/Todolist/static/js/main.1c865a8f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Todolist/static/js/main.1c865a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6425df083131b829db48",
+    "url": "/Todolist/static/js/runtime-main.8526cd1e.js"
+  }
+]);
